12d7b97f8c3c12bd1092cacbccb4e972e1bb6516
Pablo Iglesias <p.iglesias@globant.com>
2024-04-01 19:29:56 -0300

    git init clase 5

---
+++ Clase5/04-prueba/app.js
@@ -0,0 +1,37 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//importamos las rutas de la app
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.set('registerPartials', path.join(__dirname, '/views/partials'));
+
+//utilizamos middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//importamos las funciones de la app
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//pagina de error
+app.get('*', (req, res) => {
+  res.render('error', {
+    title: 'Error', 
+    message: 'Pagina no encontrada'
+  });
+})
+
+module.exports = app;
