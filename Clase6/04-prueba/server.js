0185a12d99710ec6c022506af79f2e379e1cc72f
Pablo Iglesias <p.iglesias@globant.com>
2024-04-06 12:27:31 -0300

    Clase6

--- Clase6/04-prueba/server.js
+++ Clase6/04-prueba/server.js
@@ -1,8 +1,12 @@
 const app = require('./app.js');
 const dotenv = require('dotenv');
+const conectarDB = require('./database/conexion.js');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+//ejecutamos la funcion de conexion a base de datos
+conectarDB();
 
 const server = app.listen(PORT, () => {
     console.log(`Server trabajando en http://localhost:${PORT}`);
